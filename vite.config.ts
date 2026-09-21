da6953a072e08111866bdb5df0986ab84968954a
n3artstudio-dev <n3artstudio@gmail.com>
2026-09-21 14:01:10 +0330

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,14 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["icons/icon-192.svg", "icons/icon-512.svg"],
-      manifest: false,
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,svg,webmanifest}"],
-        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
-        navigateFallback: "/index.html",
-        cleanupOutdatedCaches: true,
-        sourcemap: false,
-      },
-      devOptions: {
-        enabled: false,
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
